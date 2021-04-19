07688629f28478d6e0cf89647805a2ed19111df8
weiiew <weiwei.li@honeybeehub.ca>
2021-04-19 18:17:19 -0400

    replace

--- lib/main.gr.dart
+++ lib/main.gr.dart
@@ -14,35 +14,44 @@ class AppRouter extends _i1.RootStackRouter {
   @override
   final Map<String, _i1.PageFactory> pagesMap = {
     BooksListRoute.name: (entry) {
-      var queryParams = entry.routeData.queryParams;
-      var args = entry.routeData.argsAs<BooksListRouteArgs>(
-          orElse: () =>
-              BooksListRouteArgs(filter: queryParams.optString('filter')));
+      return _i1.MaterialPageX(entry: entry, child: _i2.BooksListScreen());
+    },
+    BookDetailsRoute.name: (entry) {
+      var pathParams = entry.routeData.pathParams;
+      var args = entry.routeData.argsAs<BookDetailsRouteArgs>(
+          orElse: () => BookDetailsRouteArgs(id: pathParams.getInt('id')));
       return _i1.MaterialPageX(
-          entry: entry, child: _i2.BooksListScreen(filter: args.filter));
+          entry: entry, child: _i2.BookDetailsScreen(id: args.id));
     }
   };
 
   @override
   List<_i1.RouteConfig> get routes => [
         _i1.RouteConfig(BooksListRoute.name, path: '/'),
+        _i1.RouteConfig(BookDetailsRoute.name, path: '/book/:id'),
         _i1.RouteConfig('*#redirect',
             path: '*', redirectTo: '/', fullMatch: true)
       ];
 }
 
-class BooksListRoute extends _i1.PageRouteInfo<BooksListRouteArgs> {
-  BooksListRoute({String? filter})
-      : super(name,
-            path: '/',
-            args: BooksListRouteArgs(filter: filter),
-            queryParams: {'filter': filter});
+class BooksListRoute extends _i1.PageRouteInfo {
+  const BooksListRoute() : super(name, path: '/');
 
   static const String name = 'BooksListRoute';
 }
 
-class BooksListRouteArgs {
-  const BooksListRouteArgs({this.filter});
+class BookDetailsRoute extends _i1.PageRouteInfo<BookDetailsRouteArgs> {
+  BookDetailsRoute({required int id})
+      : super(name,
+            path: '/book/:id',
+            args: BookDetailsRouteArgs(id: id),
+            params: {'id': id});
 
-  final String? filter;
+  static const String name = 'BookDetailsRoute';
+}
+
+class BookDetailsRouteArgs {
+  const BookDetailsRouteArgs({required this.id});
+
+  final int id;
 }
