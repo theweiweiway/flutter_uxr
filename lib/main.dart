d34886b508dafcb0334dda316b80d754e04a6619
weiiew <weiwei.li@honeybeehub.ca>
2021-04-24 12:28:57 -0400

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -7,49 +7,217 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter_uxr/main.gr.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(BooksApp());
 }
 
-final List<Book> books = [
-  Book('Stranger in a Strange Land', 'Robert A. Heinlein'),
-  Book('Foundation', 'Isaac Asimov'),
-  Book('Fahrenheit 451', 'Ray Bradbury'),
-];
+class Credentials {
+  final String username;
+  final String password;
 
-class Book {
-  final String title;
-  final String author;
+  Credentials(this.username, this.password);
+}
 
-  Book(this.title, this.author);
+abstract class Authentication {
+  Future<bool> isSignedIn();
+
+  Future<void> signOut();
+
+  Future<bool> signIn(String username, String password);
+}
+
+class MockAuthentication implements Authentication {
+  bool _signedIn = false;
+
+  @override
+  Future<bool> isSignedIn() async {
+    return _signedIn;
+  }
+
+  @override
+  Future<void> signOut() async {
+    _signedIn = false;
+  }
+
+  @override
+  Future<bool> signIn(String username, String password) async {
+    return _signedIn = true;
+  }
+}
+
+class AppState extends ChangeNotifier {
+  final Authentication auth;
+
+  AppState(this.auth);
+
+  Future<bool> signIn(String username, String password) async {
+    var success = await auth.signIn(username, password);
+    notifyListeners();
+    return success;
+  }
+
+  Future<void> signOut() async {
+    await auth.signOut();
+    notifyListeners();
+  }
 }
 
 // Declare routing setup
 @MaterialAutoRouter(
   replaceInRouteName: 'Screen,Route',
   routes: <AutoRoute>[
-    AutoRoute(path: "/", page: BooksListScreen),
+    AutoRoute(
+      path: "/",
+      name: 'AppStack',
+      page: EmptyRouterPage,
+      guards: [SignedInGuard],
+      children: [
+        AutoRoute(path: "", page: HomeScreen),
+        AutoRoute(path: "books", page: BooksListScreen),
+      ],
+    ),
+    AutoRoute(path: "/signIn", guards: [SignedOutGuard], page: SignInScreen),
     RedirectRoute(path: "*", redirectTo: "/")
   ],
 )
 class $AppRouter {}
 
-class BooksApp extends StatelessWidget {
+// Declare route guards
+class SignedOutGuard extends AutoRouteGuard {
+  final AppState appState;
+  SignedOutGuard({required this.appState});
+
+  @override
+  Future<bool> canNavigate(
+      List<PageRouteInfo> pendingRoutes, StackRouter router) async {
+    if (await appState.auth.isSignedIn()) {
+      router.root.pushPath("/");
+      return false;
+    }
+    return true;
+  }
+}
+
+class SignedInGuard extends AutoRouteGuard {
+  final AppState appState;
+  SignedInGuard({required this.appState});
+
+  @override
+  Future<bool> canNavigate(
+      List<PageRouteInfo> pendingRoutes, StackRouter router) async {
+    if (!await appState.auth.isSignedIn()) {
+      router.root.push(SignInRoute(onSignedIn: (c) {
+        appState.signIn(c.username, c.password);
+        router.replaceAll(pendingRoutes);
+      }));
+      return false;
+    }
+    return true;
+  }
+}
+
+// Main App
+class BooksApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BooksAppState();
+}
+
+class _BooksAppState extends State<BooksApp> {
+  AppState _appState = AppState(MockAuthentication());
+
+  late final _appRouter = AppRouter(
+      signedOutGuard: SignedOutGuard(appState: _appState),
+      signedInGuard: SignedInGuard(appState: _appState));
+
   @override
   Widget build(BuildContext context) {
-    final _appRouter = AppRouter();
-
     return MaterialApp.router(
-      routerDelegate: _appRouter.delegate(),
       routeInformationParser: _appRouter.defaultRouteParser(),
+      routerDelegate: _appRouter.delegate(),
+      builder: (_, router) {
+        return ChangeNotifierProvider(
+          create: (_) => _appState,
+          child: router,
+        );
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => context.router.push(BooksListRoute()),
+              child: Text('View my bookshelf'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                context.read<AppState>().signOut();
+                context.router.root.push(SignInRoute(onSignedIn: (c) {
+                  context.read<AppState>().signIn(c.username, c.password);
+                  context.router.root.pop();
+                }));
+              },
+              child: Text('Sign out'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignInScreen extends StatefulWidget {
+  final ValueChanged<Credentials> onSignedIn;
+
+  SignInScreen({required this.onSignedIn});
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  String _username = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(hintText: 'username (any)'),
+              onChanged: (s) => _username = s,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'password (any)'),
+              obscureText: true,
+              onChanged: (s) => _password = s,
+            ),
+            ElevatedButton(
+              onPressed: () =>
+                  widget.onSignedIn(Credentials(_username, _password)),
+              child: Text('Sign in'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class BooksListScreen extends StatelessWidget {
-  final String? filter;
-  BooksListScreen({@QueryParam('filter') this.filter});
+  BooksListScreen();
 
   @override
   Widget build(BuildContext context) {
@@ -57,18 +225,18 @@ class BooksListScreen extends StatelessWidget {
       appBar: AppBar(),
       body: ListView(
         children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'filter',
-            ),
-            onSubmitted: (value) => context.router.pushPath('/?filter=$value'),
+          ListTile(
+            title: Text('Stranger in a Strange Land'),
+            subtitle: Text('Robert A. Heinlein'),
           ),
-          for (var book in books)
-            if (filter == null || book.title.toLowerCase().contains(filter!))
-              ListTile(
-                title: Text(book.title),
-                subtitle: Text(book.author),
-              )
+          ListTile(
+            title: Text('Foundation'),
+            subtitle: Text('Isaac Asimov'),
+          ),
+          ListTile(
+            title: Text('Fahrenheit 451'),
+            subtitle: Text('Ray Bradbury'),
+          ),
         ],
       ),
     );
--- lib/main.gr.dart
+++ lib/main.gr.dart
@@ -9,40 +9,75 @@ import 'package:auto_route/auto_route.dart' as _i1;
 import 'main.dart' as _i2;
 
 class AppRouter extends _i1.RootStackRouter {
-  AppRouter();
+  AppRouter({required this.signedInGuard, required this.signedOutGuard});
+
+  final _i2.SignedInGuard signedInGuard;
+
+  final _i2.SignedOutGuard signedOutGuard;
 
   @override
   final Map<String, _i1.PageFactory> pagesMap = {
-    BooksListRoute.name: (entry) {
-      var queryParams = entry.routeData.queryParams;
-      var args = entry.routeData.argsAs<BooksListRouteArgs>(
-          orElse: () =>
-              BooksListRouteArgs(filter: queryParams.optString('filter')));
+    AppStack.name: (entry) {
       return _i1.MaterialPageX(
-          entry: entry, child: _i2.BooksListScreen(filter: args.filter));
+          entry: entry, child: const _i1.EmptyRouterPage());
+    },
+    SignInRoute.name: (entry) {
+      var args = entry.routeData.argsAs<SignInRouteArgs>();
+      return _i1.MaterialPageX(
+          entry: entry, child: _i2.SignInScreen(onSignedIn: args.onSignedIn));
+    },
+    HomeRoute.name: (entry) {
+      return _i1.MaterialPageX(entry: entry, child: _i2.HomeScreen());
+    },
+    BooksListRoute.name: (entry) {
+      return _i1.MaterialPageX(entry: entry, child: _i2.BooksListScreen());
     }
   };
 
   @override
   List<_i1.RouteConfig> get routes => [
-        _i1.RouteConfig(BooksListRoute.name, path: '/'),
+        _i1.RouteConfig(AppStack.name, path: '/', guards: [
+          signedInGuard
+        ], children: [
+          _i1.RouteConfig(HomeRoute.name, path: ''),
+          _i1.RouteConfig(BooksListRoute.name, path: 'books')
+        ]),
+        _i1.RouteConfig(SignInRoute.name,
+            path: '/signIn', guards: [signedOutGuard]),
         _i1.RouteConfig('*#redirect',
             path: '*', redirectTo: '/', fullMatch: true)
       ];
 }
 
-class BooksListRoute extends _i1.PageRouteInfo<BooksListRouteArgs> {
-  BooksListRoute({String? filter})
-      : super(name,
-            path: '/',
-            args: BooksListRouteArgs(filter: filter),
-            queryParams: {'filter': filter});
+class AppStack extends _i1.PageRouteInfo {
+  const AppStack({List<_i1.PageRouteInfo>? children})
+      : super(name, path: '/', initialChildren: children);
 
-  static const String name = 'BooksListRoute';
+  static const String name = 'AppStack';
 }
 
-class BooksListRouteArgs {
-  const BooksListRouteArgs({this.filter});
+class SignInRoute extends _i1.PageRouteInfo<SignInRouteArgs> {
+  SignInRoute({required void Function(_i2.Credentials) onSignedIn})
+      : super(name,
+            path: '/signIn', args: SignInRouteArgs(onSignedIn: onSignedIn));
 
-  final String? filter;
+  static const String name = 'SignInRoute';
+}
+
+class SignInRouteArgs {
+  const SignInRouteArgs({required this.onSignedIn});
+
+  final void Function(_i2.Credentials) onSignedIn;
+}
+
+class HomeRoute extends _i1.PageRouteInfo {
+  const HomeRoute() : super(name, path: '');
+
+  static const String name = 'HomeRoute';
+}
+
+class BooksListRoute extends _i1.PageRouteInfo {
+  const BooksListRoute() : super(name, path: 'books');
+
+  static const String name = 'BooksListRoute';
 }
