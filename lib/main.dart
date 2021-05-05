d2fe77df451f4ace715711eaeb4dfc875e747485
weiiew <weiwei.li@honeybeehub.ca>
2021-05-05 17:53:24 -0400

    udpate

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,9 @@
 /// Nested example
 /// Done using AutoRoute
 import 'package:auto_route/auto_route.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_uxr/main.gr.dart';
-
-import 'main.gr.dart';
 
 void main() {
   runApp(BooksApp());
